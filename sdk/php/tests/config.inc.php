a2fc36ae8fe06bff69316ee5f409a6657a49b412
Marcel Kloubert <marcel.kloubert@gmx.net>
2016-11-13 16:00:33 +0100

    moved PHP SDK to https://github.com/mkloubert/php-remote-debugger

--- sdk/php/tests/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('MJK_REMOTE_DBG_TEST') or die();
-
-# the Composer way (much better!)
-// require __DIR__ . '/../vendor/autoload.php';
-
-require __DIR__ . '/../src/MJK/Diagnostics/RemoteDebugger.php';
